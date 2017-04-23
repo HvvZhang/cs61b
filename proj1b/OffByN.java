3b9ef41261266972a0892b2416c368a30bc970ba
bananabrick <a26nair@edu.uwaterloo.ca>
2017-04-23 00:36:52 -0400

    added constructor to OffByN

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -4,12 +4,18 @@
  */
 public class OffByN implements CharacterComparator {
 
+    public int N;
+
+    public OffByN(int N) {
+        this.N = N;
+    }
+
     /**
      * Returns true if the ASCII
      * value of the chars differs by N.
      */
     @Override
     public boolean equalChars(char x, char y) {
-
+        return Math.abs(y - x) == N;
     }
 }
