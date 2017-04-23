8783c1c0e74ab11d241c9b7c6dccce2489556e6d
bananabrick <a26nair@edu.uwaterloo.ca>
2017-04-23 10:42:33 -0400

    cleaned up some logic in TestArrayDeque1B

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -17,43 +17,36 @@ public class TestArrayDeque1B {
         /* Instantiating object to keep track of operations performed. */
         OperationSequence performed = new OperationSequence();
 
-        boolean fail = false; // flag keeps track of success
-
-        while (!fail) {
-            double prob = StdRandom.uniform();
+        while (true) {
+            double prob = StdRandom.uniform(4);
             boolean shouldBeEmpty = C.size() == 0;
             Integer randomInt = StdRandom.uniform(100000);
             Integer correct, incorrect;
             DequeOperation operation;
 
-            if (prob < 0.5 && !shouldBeEmpty) {
-                if (prob < 0.25) {
-                    operation = new DequeOperation("removeFirst");
-                    correct = C.removeFirst();
-                    incorrect = I.removeFirst();
-                } else {
-                    operation = new DequeOperation("removeLast");
-                    correct = C.removeLast();
-                    incorrect = I.removeLast();
-                }
+            if (prob == 0 && !shouldBeEmpty) {
+                operation = new DequeOperation("removeFirst");
+                correct = C.removeFirst();
+                incorrect = I.removeFirst();
+            } else if (prob == 1 && !shouldBeEmpty) {
+                operation = new DequeOperation("removeLast");
+                correct = C.removeLast();
+                incorrect = I.removeLast();
+            } else if (prob == 2) {
+                operation = new DequeOperation("addFirst", randomInt);
+                C.addFirst(randomInt);
+                I.addFirst(randomInt);
+                correct = C.get(0);
+                incorrect = I.get(0);
             } else {
-                if (prob < 0.75) {
-                    operation = new DequeOperation("addFirst", randomInt);
-                    C.addFirst(randomInt);
-                    I.addFirst(randomInt);
-                    correct = C.get(0);
-                    incorrect = I.get(0);
-                } else {
-                    operation = new DequeOperation("addLast", randomInt);
-                    C.addLast(randomInt);
-                    I.addLast(randomInt);
-                    correct = C.get(C.size() - 1);
-                    incorrect = I.get(C.size() - 1);
-                }
+                operation = new DequeOperation("addLast", randomInt);
+                C.addLast(randomInt);
+                I.addLast(randomInt);
+                correct = C.get(C.size() - 1);
+                incorrect = I.get(C.size() - 1);
             }
 
             performed.addOperation(operation);
-            fail = !correct.equals(incorrect);
             assertEquals(performed.toString(), correct, incorrect);
         }
     }
