e0033226d690661feb4ee84220204c0b07748525
bananabrick <a26nair@edu.uwaterloo.ca>
2017-04-23 00:32:53 -0400

    beginning last problem

---
+++ proj1b/OffByN.java
@@ -0,0 +1,14 @@
+/**
+ * Created by arjunnair on 2017-04-23.
+ */
+public class OffByN implements CharacterComparator {
+
+    /**
+     * Returns true if the ASCII
+     * value of the chars differs by N.
+     */
+    @Override
+    public boolean equalChars(char x, char y) {
+
+    }
+}
