70a05f45dc3d3a5ebf88dc62a2416a1df7822f1d
bananabrick <a26nair@edu.uwaterloo.ca>
2017-04-27 23:33:19 -0400

    completed the implementation of the Bounded queue interface.

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,7 @@
+package synthesizer;
+
+/**
+ * Created by arjunnair on 2017-04-27.
+ */
+public class AbstractBoundedQueue {
+}
---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,39 @@
+package synthesizer;
+
+/**
+ * An interface for a limited capacity queue.
+ * @author Arjun Nair
+ */
+public interface BoundedQueue<T> {
+
+    /** Returns the maximum capacity of the queue. */
+    int capacity();
+
+    /** Returns the number of items currently in the queue. */
+    int fillCount();
+
+    /**
+     * Adds an item to the end of the queue.
+     * @param x The item to be added to the queue.
+     */
+    void enqueue(T x);
+
+    /** Removes an item from the front of the queue. */
+    T deque();
+
+    /**
+     * Returns the item at the front of the queue.
+     * Does not delete it.
+     */
+    T peek();
+
+    /** Returns true if the queue is empty. */
+    default boolean isEmpty() {
+        return this.fillCount() == 0;
+    }
+
+    /** Returns true if the queue is full. */
+    default boolean isFull() {
+        return this.fillCount() == this.capacity();
+    }
+}
